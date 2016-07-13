7a0ccf6c0f980263bd6701fc6f59fe7931959cb6
perlancar (@pc-office) <perlancar@gmail.com>
2016-07-13 08:22:22 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/Images.pm
@@ -0,0 +1,16 @@
+package Acme::PERLANCAR::Test::Images;
+
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Distribution that contains images
+
+=head1 DESCRIPTION
+
+This distribution contains images in the F<share/images> directory.
